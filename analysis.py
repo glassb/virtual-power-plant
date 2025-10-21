4c9f0247a5c169e0fdfa7f94e4d310a511138d75
Ben Glass <glassb@users.noreply.github.com>
2025-10-21 02:02:07 -0400

    Analysis file
    Created Analysis.py, created resetSimulation function, deleted old logs, created run function in run.py

---
+++ analysis.py
@@ -0,0 +1,38 @@
+import run 
+import configuration as config
+import pandas as pd
+import functions
+import os
+
+
+
+OUTPUT_ANALYSIS_PATH = "/Users/benjaminglass/Desktop/EC601/virtual-power-plant/outputFiles"
+outputAnalysis = pd.DataFrame(columns=['Power Request','Load Scaling Factor','Cost Result'])
+
+POWER_REQUESTS = [8200,8700,9200,9700]
+SCALING_FACTORS = [.11,.13,.15,.17,.19,.21]
+
+
+for i in range(len(POWER_REQUESTS)):
+	for j in range(len(SCALING_FACTORS)):
+
+		functions.resetSimulation()
+
+		config.AGGREGATOR_POWER_REQUEST = POWER_REQUESTS[i]
+		config.LOAD_SCALING_FACTOR = SCALING_FACTORS[j]
+
+		print(config.AGGREGATOR_POWER_REQUEST)
+		print(config.LOAD_SCALING_FACTOR)
+
+		result = run.run()
+
+		#got this tip from gemini to use len(outputAnalysis) in the loc method
+		outputAnalysis.loc[len(outputAnalysis)] = [config.AGGREGATOR_POWER_REQUEST,config.LOAD_SCALING_FACTOR,result]
+		print(outputAnalysis)
+
+
+print(outputAnalysis)
+
+os.chdir(OUTPUT_ANALYSIS_PATH)
+dateTimeNow = functions.getCurrentTimeStamp()
+outputAnalysis.to_csv("analysisOutput-"+dateTimeNow+".csv")
